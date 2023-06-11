f56193d5be6e862c997fd5a5781edb2374045f58
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-06-11 14:08:44 +0500

    is after removed

--- repository/module.js
+++ repository/module.js
@@ -33,7 +33,6 @@ const book = sequelize.define("book", {
   },
   start: {
     type: DataTypes.STRING,
-    isAfter: `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}`,
     is: /[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0][0]:[0][0]/gm,
   },
   end: {
